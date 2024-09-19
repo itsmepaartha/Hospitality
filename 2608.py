a96ae448d777313fc68f4c5347fcec9d48d6813c
parthiban <saiparthiv463@gmail.com>
2024-09-19 09:51:25 +0530

    Parthi.py

---
+++ 2608.py
@@ -0,0 +1,15 @@
+name=input()
+age=input()
+adress=input()
+print("my name is", name)
+print("my age is", age)
+print("my native is",adress)
+
+
+a=int(input())
+b=int(input())
+c=int(input())
+d=a+b+c
+e=a*b*c
+f=d/e
+print(f)
